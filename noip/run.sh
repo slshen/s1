8eedd65abdcb516c0ea085cfc8807205929826b2
Sam Shen <slshen@users.noreply.github.com>
2020-04-26 18:10:44 +0000

    add noip update

---
+++ noip/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+password=$(cat /password.txt)
+
+while true; do
+  dns_addr=$(host slshen.hopto.org 1.1.1.1 | grep "slshen.hopto.org has address " | awk '{ print $NF }')
+  addr=$(curl -s ifconfig.me)
+  echo "dns_adr = $dns_addr addr = $addr"
+  if [ "$addr" != "" -a "$dns_addr" != "$addr" ]; then
+    echo "internet address is $addr"
+    echo "dns address is $dns_addr"
+    curl -u sls@metopic.com:${password} \
+      "https://dynupdate.no-ip.com/nic/update?hostname=slshen.hopto.org&myip=${ip}"
+  fi
+  sleep 600
+done
